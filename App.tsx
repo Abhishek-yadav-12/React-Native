5d883d8c82c806ecd2aed71e3198d6f21f890498
Abhishek-yadav-12 <abhishekay2003@gmail.com>
2025-02-13 22:48:26 +0530

    jsx

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import {
+  Alert,
   Button,
   Text,
   View,
@@ -9,11 +10,20 @@ import {
 
 const App = () => {
 
+  const name = 'Abhishek Yadav';
+  let age = 21;
+  var email = 'abhishekay2003@gmail.com';
+
   return(
     <View>
       <Text style={{fontSize:40}}> Learn Native with Abhishekkkkkk </Text>
+      <Button title="Click me" onPress={() => Alert.alert('Button pressed!')} />
+      <Text style={{fontSize:20}}> {name}</Text>
+      <Text style={{fontSize:20}}> {age}</Text>
+      <Text style={{fontSize:20}}> {email}</Text>
       <Button title='Get Started'></Button>
     </View>
+    
 
   );
 };
